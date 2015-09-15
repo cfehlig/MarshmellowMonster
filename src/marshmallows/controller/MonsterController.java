50ada6a3c0374bcbb0fe5124ba89bc32d00e878b
cfehlig <cfeh7454@740s-pl-126.local>
2015-09-15 09:51:42 -0600

    added questions

--- src/marshmallows/controller/MonsterController.java
+++ src/marshmallows/controller/MonsterController.java
@@ -1,7 +1,8 @@
 package marshmallows.controller;
 
 import marshmallows.model.MarshmallowMonster;
-import marshmallow.view.Marshmalloutput;
+import marshmallows.view.Marshmallowoutput;
+import java.util.Scanner;
 
 public class MonsterController
 {
@@ -17,6 +18,7 @@ public class MonsterController
 		boolean hasBellyButton = false;
 		String name ="Happy Monster";
 		
+		monsterScanner = new Scanner(System.in);
 		myOutput = new MarshmallowOutput();
 		cassandraMonster = new MarshmallowMonster(name, eyes, noses, hair, legs, hasBellyButton);
 	}
@@ -24,11 +26,18 @@ public class MonsterController
 	public void start()
 	{
 		myOutput.displayMonsterInfo(cassandraMonster.toString());
-		ask questions();
-		myOUtput.displayMonsterInfo("New MOnster Info" + casdraMonster.toString());
+		askQuestions();
+		myOutput.displayMonsterInfo("New Monster Info" + cassandraMonster.toString());
 	}
 	private void askQuestions()
 	{
-		
+		System.out.println("Type in a better name for the monster");
+		String betterMonsterName = monsterScanner.next();
+		cassandraMonster.setMonsterName(betterMonsterName);
+		System.out.println("Type in a different number of eyes for the monster");
+		int betterMonsterEyes = monsterScanner.nextInt();
+		cassandraMonster.setMonsterEyes(betterMonsterEyes);
+		System.println("Type in a different number of legs for the monster");
+		double betterMonsterLegs = monsterScanner.nextdouble();
 	}
 }
